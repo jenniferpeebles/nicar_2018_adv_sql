1f44ed8aff43cf4099ef8eab93e92f8003e3b262
Jennifer Peebles <jenniferpeebles@users.noreply.github.com>
2018-03-09 08:58:03 -0500

    Word wrap is really on my nerves now

--- sql_for_nicar_class.sql
+++ sql_for_nicar_class.sql
@@ -1,5 +1,6 @@
 /* First of all, thank you to Liz Lucas, who has taught this class in previous years, for posting her code here on Github. 
-I have shamelessly copied it and tried to add on to it. Liz has the original code and the OSHA data for the class at https://github.com/eklucas/NICAR-Adv-SQL. */
+I have shamelessly copied it and tried to add on to it. Liz has the original code and the OSHA data for the class at 
+https://github.com/eklucas/NICAR-Adv-SQL. */
 
 /* So, let's first make sure all our records loaded and find out how many records are in our table if we don't know already. */
 
@@ -28,7 +29,8 @@ FROM osha_inspection
 LIMIT 2000000
 ;
 
-/* Let's poke around a little more and get an idea of what's in the data and see if we can get a feel for how clean, or dirty, it might be. Let's look at the state field as an example. */
+/* Let's poke around a little more and get an idea of what's in the data and see if we can get a feel for how clean, or dirty, 
+it might be. Let's look at the state field as an example. */
 
 SELECT site_state, COUNT(*) AS number_of_this_state
 FROM osha_inspection
